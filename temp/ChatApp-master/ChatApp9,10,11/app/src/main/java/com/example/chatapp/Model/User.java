aa6867d789d319abc9f0465438644e033c92318f
duli4 <duli@oregonstate.edu>
2019-03-12 01:12:43 -0700

    ChatApp

---
+++ temp/ChatApp-master/ChatApp9,10,11/app/src/main/java/com/example/chatapp/Model/User.java
@@ -0,0 +1,54 @@
+package com.example.chatapp.Model;
+
+public class User {
+
+    private String id;
+    private String username;
+    private String imageURL;
+    private String status;
+
+    public User(String id, String username, String imageUrl, String status){
+        this.id=id;
+        this.username=username;
+        this.imageURL=imageUrl;
+        this.status = status;
+    }
+
+
+    public User(){
+
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageURL = imageUrl;
+    }
+
+    public String getImageUrl() {
+        return imageURL;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
